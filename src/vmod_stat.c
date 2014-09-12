c21807f4e9c4c06a1e6699c9704f5c643c2468a3
Frederic Le Bris <flebris@gmail.com>
2014-09-12 12:04:38 +0200

    premier version

--- src/vmod_stat.c
+++ src/vmod_stat.c
@@ -6,6 +6,9 @@
 
 #include "vcc_if.h"
 
+int getVisitorId(char * ad_ip, char* visitorId);
+int getTimestamp(char* timestamp);
+
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
@@ -13,14 +16,21 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 }
 
 VCL_STRING
-vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
+vmod_getvisitorid(const struct vrt_ctx *ctx, VCL_STRING ad_ip)
 {
 	char *p;
 	unsigned u, v;
-
+	
+        char visitorId[18];
+        memset( (char*) visitorId, 0, 18);
+	
 	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
 	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
+
+	getVisitorId((char*)ad_ip, (char*)visitorId);
+
+	v = snprintf(p, u, "%s", visitorId);
+
 	v++;
 	if (v > u) {
 		/* No space, reset and leave */
@@ -31,3 +41,54 @@ vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
 	WS_Release(ctx->ws, v);
 	return (p);
 }
+
+int getVisitorId(char * ad_ip, char* visitorId) {
+
+        int random;
+        char timestamp[13];
+        memset( (char*) timestamp, 0, 13);
+
+
+        getTimestamp( (char*) timestamp );
+
+        //printf( "Retour methode : %s \n", timestamp);
+
+        srand(time(NULL));
+        random = rand() % 99 + 1;
+
+        //printf( "Retour random : %02i \n", random);
+
+        sprintf(visitorId, "%s%s%02i", (char*)ad_ip,timestamp, random);
+
+       // printf("visitorId = %s \n", visitorId);
+        return 0;
+}
+
+
+int getTimestamp(char* timestamp) {
+
+        struct timespec tms;
+
+    /* The C11 way */
+    /* if (! timespec_get(&tms, TIME_UTC)) { */
+
+    /* POSIX.1-2008 way */
+    if (clock_gettime(CLOCK_REALTIME,&tms)) {
+        return -1;
+    }
+    /* seconds, multiplied with 1000  */
+    int64_t millis = tms.tv_sec * 1000;
+    millis += tms.tv_nsec/1000;
+    /* round up if necessary */
+    if (tms.tv_nsec % 1000 >= 500) {
+        ++millis;
+    }
+    //printf("Milliseconds: %"PRId64"\n",millis);
+
+    sprintf((char*) timestamp, "%lld", millis);
+
+    return 1;
+
+}
+
+
