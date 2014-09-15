0a247cdcb7ca47c5160ee66298f805a926eb8798
Frederic Le Bris <flebris@gmail.com>
2014-09-15 12:51:29 +0200

    add parseip

--- src/vmod_stat.c
+++ src/vmod_stat.c
@@ -8,12 +8,76 @@
 
 int getVisitorId(char * ad_ip, char* visitorId);
 int getTimestamp(char* timestamp);
+int getip( char* ip, char* res);
+
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
 	return (0);
 }
+
+VCL_STRING
+vmod_gettimestamp(const struct vrt_ctx *ctx)
+{
+        char *p;
+        unsigned u, v;
+
+        char timestamp[14];
+        memset( (char*) timestamp, 0, 14);
+
+        u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
+        p = ctx->ws->f;         /* Front of workspace area */
+
+        getTimestamp((char*) timestamp);
+
+        v = snprintf(p, u, "%s", timestamp);
+
+        v++;
+        if (v > u) {
+                /* No space, reset and leave */
+                WS_Release(ctx->ws, 0);
+                return (NULL);
+        }
+        /* Update work space with what we've used */
+        WS_Release(ctx->ws, v);
+        return (p);
+}
+
+
+VCL_STRING
+vmod_parseip(const struct vrt_ctx *ctx, VCL_STRING ad_ip)
+{
+
+	char *p;
+        unsigned u, v;
+
+        char res[4];
+        char ip[16];
+        memset( (char*) res, 0, 4);
+        memset( (char*) ip, 0, 16);
+
+	strncpy(ip, ad_ip, 16 );
+
+        u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
+        p = ctx->ws->f;         /* Front of workspace area */
+
+        getip((char*) ip, (char*) res);
+
+        v = snprintf(p, u, "%s", res);
+
+        v++;
+        if (v > u) {
+                /* No space, reset and leave */
+                WS_Release(ctx->ws, 0);
+                return (NULL);
+        }
+        /* Update work space with what we've used */
+        WS_Release(ctx->ws, v);
+        return (p);
+
+}
+
 
 VCL_STRING
 vmod_getvisitorid(const struct vrt_ctx *ctx, VCL_STRING ad_ip)
@@ -41,6 +105,25 @@ vmod_getvisitorid(const struct vrt_ctx *ctx, VCL_STRING ad_ip)
 	WS_Release(ctx->ws, v);
 	return (p);
 }
+
+int getip( char* ad_ip, char* res){
+
+  	char * pch;
+        pch = strtok (ad_ip,".");
+        int i=1;
+        while (pch != NULL)
+        {
+                i++;
+                pch= strtok (NULL, ".");
+                if (i==4) break;
+        }
+        if (strlen(pch) == 1 ) sprintf(res, "00%s", pch);
+        if (strlen(pch) == 2 ) sprintf(res, "0%s", pch);
+        if (strlen(pch) == 3 ) sprintf(res, "%s", pch);
+
+	return 0;
+}
+
 
 int getVisitorId(char * ad_ip, char* visitorId) {
 
